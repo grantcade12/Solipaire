d3f2045a07307467704e7fa6c0a0d61ab64d7b8e
grantcade12 <grant.633@osu.edu>
2022-10-07 15:23:45 -0400

    added logging

--- app/src/main/java/com/example/solipaire/StartScreenActivity.java
+++ app/src/main/java/com/example/solipaire/StartScreenActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class StartScreenActivity extends AppCompatActivity {
 
@@ -14,6 +15,7 @@ public class StartScreenActivity extends AppCompatActivity {
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(null,"StartScreenActivity onCreate() started");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
         FragmentManager fm = getSupportFragmentManager();
@@ -24,11 +26,42 @@ public class StartScreenActivity extends AppCompatActivity {
                     .add(R.id.fragment_container, fragment)
                     .commit();
         }
+        Log.i(null,"StartScreenActivity onCreate() complete");
+    }
+
+    @Override
+    public void onResume() {
+        Log.i(null,"StartScreenActivity onResume() started");
+        super.onResume();
+        Log.i(null,"StartScreenActivity onResume() complete");
+    }
+
+    @Override
+    public void onStart() {
+        Log.i(null,"StartScreenActivity onStart() started");
+        super.onStart();
+        Log.i(null,"StartScreenActivity onStart() complete");
+    }
+
+    @Override
+    public void onStop() {
+        Log.i(null,"StartScreenActivity onStop() started");
+        super.onStop();
+        Log.i(null,"StartScreenActivity onStop() complete");
+    }
+
+    @Override
+    public void onPause() {
+        Log.i(null,"StartScreenActivity onPause() started");
+        super.onPause();
+        Log.i(null,"StartScreenActivity onPause() complete");
     }
 
     @Override
     public void onDestroy() {
+        Log.i(null,"StartScreenActivity onDestroy() started");
         super.onDestroy();
+        Log.i(null,"StartScreenActivity onDestroy() complete");
     }
 
 
--- app/src/main/java/com/example/solipaire/StartScreenFragment.java
+++ app/src/main/java/com/example/solipaire/StartScreenFragment.java
@@ -2,6 +2,7 @@ package com.example.solipaire;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +15,15 @@ public class StartScreenFragment extends Fragment implements View.OnClickListene
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Log.i(null,"StartScreenFragment onCreate() started");
         super.onCreate(savedInstanceState);
         Activity activity = requireActivity();
+        Log.i(null,"StartScreenFragment onCreate() complete");
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        Log.i(null,"StartScreenFragment onCreateView() started");
         View v;
         Activity activity = requireActivity();
         v = inflater.inflate(R.layout.fragment_startscreen, container, false);
@@ -33,22 +37,64 @@ public class StartScreenFragment extends Fragment implements View.OnClickListene
         if (loginButton != null) {
             loginButton.setOnClickListener(this);
         }
+        Log.i(null,"StartScreenFragment onCreateView() complete");
         return v;
     }
 
     @Override
+    public void onResume() {
+        Log.i(null,"StartScreenFragment onResume() started");
+        super.onResume();
+        Log.i(null,"StartScreenFragment onResume() complete");
+    }
+
+    @Override
+    public void onStart() {
+        Log.i(null,"StartScreenFragment onStart() started");
+        super.onStart();
+        Log.i(null,"StartScreenFragment onStart() complete");
+    }
+
+    @Override
+    public void onStop() {
+        Log.i(null,"StartScreenFragment onStop() started");
+        super.onStop();
+        Log.i(null,"StartScreenFragment onStop() complete");
+    }
+
+    @Override
+    public void onPause() {
+        Log.i(null,"StartScreenFragment onPause() started");
+        super.onPause();
+        Log.i(null,"StartScreenFragment onPause() complete");
+    }
+
+    @Override
     public void onDestroy() {
+        Log.i(null,"StartScreenFragment onDestroy() started");
         super.onDestroy();
+        Log.i(null,"StartScreenFragment onDestroy() complete");
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.i(null,"StartScreenFragment onDestroyView() started");
+        super.onDestroyView();
+        Log.i(null, "StartScreenFragment onDestroyView() complete");
     }
 
     @Override
     public void onClick(View view) {
+        Log.i(null,"StartScreenFragment onClick() started");
         final Activity activity = requireActivity();
         final int viewId = view.getId();
         if (viewId == R.id.LogInButton){
             //GO TO LOGIN SCREEN, DO NOTHING FOR NOW
+            Log.i(null,"StartScreenFragment onClick() LogInButton clicked");
         } else if (viewId == R.id.SignUpButton){
             //GO TO CREATE ACCOUNT SCREEN, DO NOTHING FOR NOW
+            Log.i(null,"StartScreenFragment onClick() SignUpButton clicked");
         }
+        Log.i(null,"StartScreenFragment onClick() started");
     }
 }
