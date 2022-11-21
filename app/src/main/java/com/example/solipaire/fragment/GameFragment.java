c987712d7556068cbbc088bca3e09387b79af403
Jelli687 <90652510+Jelli687@users.noreply.github.com>
2022-11-21 11:03:21 -0500

    Adding share button to the end of the game

--- app/src/main/java/com/example/solipaire/fragment/GameFragment.java
+++ app/src/main/java/com/example/solipaire/fragment/GameFragment.java
@@ -5,6 +5,8 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -29,6 +31,12 @@ import com.example.solipaire.data.Player;
 import com.example.solipaire.viewmodel.AccountViewModel;
 import com.example.solipaire.viewmodel.GameViewModel;
 import com.example.solipaire.viewmodel.PlayerViewModel;
+import com.facebook.CallbackManager;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareButton;
+import com.facebook.share.widget.ShareDialog;
 
 import java.util.Locale;
 
@@ -43,6 +51,12 @@ public class GameFragment extends Fragment implements View.OnClickListener {
     private Bundle savedInstanceState;
     private GameViewModel gameViewModel;
     private PlayerViewModel playerViewModel;
+    SharePhotoContent content;
+
+    Bitmap image;
+
+    CallbackManager callbackManager;
+    ShareDialog shareDialog;
 
 
     @Override
@@ -51,6 +65,9 @@ public class GameFragment extends Fragment implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         Context context = requireContext();
         Activity activity = requireActivity();
+        image = BitmapFactory.decodeResource(getResources(),R.drawable.solipairelogo);
+        callbackManager = CallbackManager.Factory.create();
+        shareDialog = new ShareDialog(this);
         playerViewModel = new ViewModelProvider((ViewModelStoreOwner) activity).get(PlayerViewModel.class);
         gameViewModel = new ViewModelProvider((ViewModelStoreOwner) activity).get(GameViewModel.class);
         prefs = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
@@ -68,6 +85,14 @@ public class GameFragment extends Fragment implements View.OnClickListener {
         } else {
             v = inflater.inflate(R.layout.fragment_game, container, false);
         }
+        SharePhoto photo = new SharePhoto.Builder()
+                .setBitmap(image)
+                .build();
+        content = new SharePhotoContent.Builder()
+                .addPhoto(photo)
+                .build();
+        ShareButton shareButton = v.findViewById(R.id.statsShareBtn);
+        shareButton.setShareContent(content);
 
         createNewGame(v);
 
@@ -77,8 +102,8 @@ public class GameFragment extends Fragment implements View.OnClickListener {
 
     private void createNewGame(View v) {
         gameBoard = v.findViewById(R.id.GameBoard);
-        p1id = "a";
-        p2id = "b";
+        p1id = "1";
+        p2id = "2";
         int id = 0;
         game = new Game(prefs.getInt("Id", id), p1id, p2id);
         Player player1 = new Player(p1id, prefs.getInt("Id", id));
@@ -155,6 +180,9 @@ public class GameFragment extends Fragment implements View.OnClickListener {
         new AlertDialog.Builder(activity)
                 .setTitle(endMessage)
                 .setMessage("Player " + p1Name + " Score: " + p1Score  + "\n" + "Player " + p2Name + " Score: " + p2Score)
+                .setNeutralButton("Share", (dialog, which) -> {
+                    ShareDialog.show(activity, content);
+                })
                 .setNegativeButton("Exit", (dialog, which) -> {
                     activity.finish();
                 })
